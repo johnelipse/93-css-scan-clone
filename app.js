465d86c461cb0817bc322c6509bdffc476c1ff25
johnelipse <158051619+johnelipse@users.noreply.github.com>
2024-03-21 12:41:01 +0300

    additional js commands

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const allBtns=document.querySelector(".allBtns")
 const buttonsToCopy=document.querySelectorAll("#buttonToCopy")
+const overlay=document.querySelector(".overlay")
+const shareForm=document.querySelector(".shareForm")
+const shareButton=document.querySelector(".btn-4")
+const doneBtn=document.querySelector("#doneBtn")
+const input=document.querySelector("#input")
+const copyContent=document.querySelector("#copyContent")
+console.log(copyContent);
 // console.log(buttonToCopy);
 buttonsToCopy.forEach((buttonToCopy) => {
     buttonToCopy.addEventListener("click", function () {
@@ -12,3 +19,21 @@ buttonsToCopy.forEach((buttonToCopy) => {
       }
     });
   });
+shareButton.addEventListener("click",function(){
+  shareForm.style.display="block"
+  overlay.style.display="block"
+})
+doneBtn.addEventListener("click",function(){
+  shareForm.style.display="none"
+  overlay.style.display="none"
+})
+input.textContent="index.html"
+const contentToCopy=input.textContent
+copyContent.addEventListener("click",function(){
+  if(navigator.clipboard.writeText(contentToCopy)){
+    copyContent.textContent="copied"
+    setTimeout(()=>{
+      copyContent.textContent="copy"
+    },2000)
+  }
+})
